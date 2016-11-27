5b3f4c0a82ee8761b09d9a35d604caf273e3debe
mokhles-zribi <zribi.mokhles@gmail.com>
2016-11-27 11:03:36 +0100

    act terrain

--- app/src/main/java/com/devfest/a3m/SecendActivity.java
+++ app/src/main/java/com/devfest/a3m/SecendActivity.java
@@ -1,13 +1,32 @@
 package com.devfest.a3m;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SecendActivity extends AppCompatActivity {
+public class SecendActivity extends AppCompatActivity  {
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secend);
+    }
+
+    public void goToResr(View view) {
+        i = new Intent(getApplicationContext(),ReservationActivity.class);
+        startActivity(i);
+    }
+
+
+    public void goToRechContre(View view) {
+        i = new Intent(getApplicationContext(),RechercheContre.class);
+        startActivity(i);
+    }
+
+    public void goToRechPos(View view) {
+        i = new Intent(getApplicationContext(),RecherchePosition.class);
+        startActivity(i);
     }
 }
